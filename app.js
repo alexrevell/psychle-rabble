94edcb3b4b906324ab8c213ea26e4100b7b6eed8
alexrevell <revellalex@gmail.com>
2015-08-26 07:50:00 +1200

    add simple ajax get request for fake user data to display in basic div, on button click

--- app.js
+++ app.js
@@ -9,19 +9,34 @@ function main(responses) {
 
   const USERS_URL = "http://jsonplaceholder.typicode.com/users/"
 
-  // let getRandomUser$ = responses.DOM.get
-
-  let getRandomUser$ = click$.map(() => {
-    let randomNum = Math.round(Math.Random()*9)+1
+  let getRandomUser$ = responses.DOM.get(".get-random", "click").map(() => {
+    let randomNum = Math.round(Math.random()*9)+1
     return {
       url: USERS_URL + String(randomNum),
       method: "GET"
     }
   })
 
+  let user$ = responses.HTTP
+    .filter( res$ => res$.request.url.indexOf(USERS_URL) === 0)
+    .mergeAll()
+    .map( res => res.body )
+    .startWith(null)
+
+  let vtree$ = user$.map( user =>
+    h("div.users", [
+      h("button.get-random", "Get random youzer"),
+      user === null ? null : h("div.user-details", [
+        h("h1.user-name", user.name),
+        h("h1.user-email", user.email),
+        h("a.user-website", { href: user.website }, user.website)
+      ])
+    ])
+  )
+
   let requests = {
     DOM: responses.DOM.get("input", "change")
-      .map(ev => ev.target.checked)
+      .map( ev => ev.target.checked )
       .startWith(false)
       .map(toggled =>
         h("div.sweet-toggledog", [
@@ -31,7 +46,7 @@ function main(responses) {
       )
   }
   return {
-    DOM: requests.DOM,
+    DOM: vtree$,
     HTTP: getRandomUser$
   }
 }
