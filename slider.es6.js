ec8373ade24287a820bd735180d1420175f43b2d
alexrevell <revellalex@gmail.com>
2015-11-15 10:16:36 +1300

    refactor cycle code

--- slider.es6.js
+++ slider.es6.js
@@ -1,42 +1,56 @@
-import Rx from 'rx'
-import Cycle from '@cycle/core'
-import {h, makeDOMDriver} from '@cycle/dom'
+import Cycle from '@cycle/core';
+import {h, makeDOMDriver} from '@cycle/dom';
 
-function main({DOM}) {
-  let changeWeight$ = DOM.select('#weight').events('input')
-    .map(ev => ev.target.value)
-  let changeHeight$ = DOM.select('#height').events('input')
-    .map(ev => ev.target.value)
-  let state$ = Rx.Observable.combineLatest(
+function renderWeightSlider(weight) {
+  return h('div', [
+    'Weight ' + weight + 'kg',
+    h('input#weight', {type: 'range', min: 40, max: 140, value: weight})
+  ]);
+}
+
+function renderHeightSlider(height) {
+  return h('div', [
+    'Height ' + height + 'cm',
+    h('input#height', {type: 'range', min: 140, max: 210, value: height})
+  ]);
+}
+
+function calculateBMI(weight, height) {
+  let heightMeters = height * 0.01;
+  return Math.round(weight / (heightMeters * heightMeters));
+}
+
+// NEW!
+function model(changeWeight$, changeHeight$) {
+  return Cycle.Rx.Observable.combineLatest(
     changeWeight$.startWith(70),
     changeHeight$.startWith(170),
-    (weight, height) => {
-      let heightMeters = height * 0.01
-      let bmi = Math.round(weight / (heightMeters * heightMeters))
-      console.log(weight, height, bmi)
-      return {weight, height, bmi}
-    }
-  )
+    (weight, height) =>
+      ({weight, height, bmi: calculateBMI(weight, height)})
+  );
+}
+
+function view(state$) {
+  return state$.map(({weight, height, bmi}) =>
+    h('div', [
+      renderWeightSlider(weight),
+      renderHeightSlider(height),
+      h('h2', 'BMI is ' + bmi)
+    ])
+  );
+}
+
+// UPDATED!
+function main({DOM}) {
+  let changeWeight$ = DOM.select('#weight').events('input')
+    .map(ev => ev.target.value);
+  let changeHeight$ = DOM.select('#height').events('input')
+    .map(ev => ev.target.value);
+  let state$ = model(changeWeight$, changeHeight$);
 
   return {
-    DOM: state$.map(({weight, height, bmi})  =>
-      h('div', [
-        h('div', [
-          `Weight ${weight} kg`,
-          h('div', [
-            h('input#weight', {type: 'range', min: 40, max: 140})
-          ])
-        ]),
-        h('div', [
-          `Height ${height} cm`,
-          h('div', [
-            h('input#height', {type: 'range', min: 140, max: 210})
-          ])
-        ]),
-        h('h2', `BMI is ${bmi}`)
-      ])
-    )
-  }
+    DOM: view(state$)
+  };
 }
 
 Cycle.run(main, {
