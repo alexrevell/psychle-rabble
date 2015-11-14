829510c6f00ea416a8dd9692ee25779902ced4ed
alexrevell <revellalex@gmail.com>
2015-11-15 10:31:35 +1300

    remove last pesky semi colons, refactor main into sweet function combo

--- slider.es6.js
+++ slider.es6.js
@@ -1,12 +1,12 @@
-import Cycle from '@cycle/core';
-import {h, makeDOMDriver} from '@cycle/dom';
+import Cycle from '@cycle/core'
+import {h, makeDOMDriver} from '@cycle/dom'
 
 function renderWeightSlider(weight) {
   return h('div', [
     'Weight ' + weight + 'kg', h('div', [
       h('input#weight', {type: 'range', min: 40, max: 140, value: weight})
     ])
-  ]);
+  ])
 }
 
 function renderHeightSlider(height) {
@@ -19,16 +19,16 @@ function renderHeightSlider(height) {
 
 function calculateBMI(weight, height) {
   let heightMeters = height * 0.01;
-  return Math.round(weight / (heightMeters * heightMeters));
+  return Math.round(weight / (heightMeters * heightMeters))
 }
 
-function model(changeWeight$, changeHeight$) {
+function model({changeWeight$, changeHeight$}) {
   return Cycle.Rx.Observable.combineLatest(
     changeWeight$.startWith(70),
     changeHeight$.startWith(170),
     (weight, height) =>
       ({weight, height, bmi: calculateBMI(weight, height)})
-  );
+  )
 }
 
 function view(state$) {
@@ -38,21 +38,22 @@ function view(state$) {
       renderHeightSlider(height),
       h('h2', 'BMI is ' + bmi)
     ])
-  );
+  )
+}
+
+function intent(DOM) {
+  return {
+    changeWeight$: DOM.select('#weight').events('input')
+    .map(ev => ev.target.value),
+    changeHeight$: DOM.select('#height').events('input')
+    .map(ev => ev.target.value)
+  }
 }
 
 function main({DOM}) {
-  let changeWeight$ = DOM.select('#weight').events('input')
-    .map(ev => ev.target.value);
-  let changeHeight$ = DOM.select('#height').events('input')
-    .map(ev => ev.target.value);
-  let state$ = model(changeWeight$, changeHeight$);
-
-  return {
-    DOM: view(state$)
-  };
+  return { DOM: view(model(intent(DOM))) }
 }
 
 Cycle.run(main, {
   DOM: makeDOMDriver('#app-1')
-});
+})
